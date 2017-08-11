82c59fe635d01c4e54c8bbd87db0115884e71aa4
David Rolfs <davidrolfs15@gmail.com>
2017-08-11 16:18:10 -0700

    create tests

---
+++ SonOfCod.Tests/ControllerTests/ControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTests
+{
+    public class MarketingControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            MarketingController controller = new MarketingController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+       // [Fact]
+     //   public void Get_ModelList_Index_Test()
+      //  {
+       //     //Arrange
+      //      ViewResult indexView = new MarketingController().Index() as ViewResult;
+
+       //     //Act
+      //      var result = indexView.ViewData.Model;
+
+      //      //Assert
+      //      Assert.IsType<List<Subscriber>>(result);
+      //  }
+    }
+}
---
+++ SonOfCod.Tests/ModelTests/Test.cs
@@ -0,0 +1,22 @@
+﻿using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests
+{
+    public class SubsciberTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var Subscriber = new Subscriber();
+            Subscriber.Name = "Paul";
+
+            //Act
+            var result = Subscriber.Name;
+
+            //Assert
+            Assert.Equal("Paul", result);
+        }
+    }
+}
